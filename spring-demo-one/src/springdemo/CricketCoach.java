e2d5597f0477220676845ec1271b68f1d58bf4b0
vntjoig <vntjoig@LW-439-7913.venturus.local>
2019-06-10 16:38:58 -0300

    Literal Values Injection

--- spring-demo-one/src/springdemo/CricketCoach.java
+++ spring-demo-one/src/springdemo/CricketCoach.java
@@ -4,13 +4,12 @@ public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	private String emailAddress;
+	private String team;
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice Fast Bowling 15 Minutes";
-	}
-	
-	public void setFortuneService(FortuneService fortuneService) {
-		this.fortuneService = fortuneService;
 	}
 
 	@Override
@@ -18,4 +17,25 @@ public class CricketCoach implements Coach {
 		return fortuneService.getFortune();
 	}
 
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+	
+	
 }
