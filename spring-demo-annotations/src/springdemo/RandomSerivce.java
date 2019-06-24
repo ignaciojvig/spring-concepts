baafbf3f3f31d3bab3017a8f40dba74a13e294f7
João Victor Ignacio <joao.ignacio@venturus.org.br>
2019-06-24 10:10:25 -0300

    Using Qualifier to specify a implementation on Dependency Injection

---
+++ spring-demo-annotations/src/springdemo/RandomSerivce.java
@@ -0,0 +1,13 @@
+package springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomSerivce implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		return null;
+	}
+
+}
