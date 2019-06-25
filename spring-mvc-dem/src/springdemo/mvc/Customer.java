ca6fa1ca23e35719783aca57c650866025a35ace
João Victor Ignacio <joao.ignacio@venturus.org.br>
2019-06-25 18:25:39 -0300

    Regex Validation

--- spring-mvc-dem/src/springdemo/mvc/Customer.java
+++ spring-mvc-dem/src/springdemo/mvc/Customer.java
@@ -3,6 +3,7 @@ package springdemo.mvc;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -17,6 +18,15 @@ public class Customer {
 	@Max(value=10, message="must be less than or equal to 10")
 	private int freePasses;
 	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+	private String postalCode;
+	
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
 	public String getFirstName() {
 		return firstName;
 	}
