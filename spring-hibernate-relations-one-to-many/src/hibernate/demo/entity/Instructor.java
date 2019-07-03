2378244b8b576cddb0ba9ecc0bac9350a3e39374
João Victor Ignacio <joao.ignacio@venturus.org.br>
2019-07-03 16:05:18 -0300

    Retrieving and inserting objects in one-to-many relationship

--- spring-hibernate-relations-one-to-many/src/hibernate/demo/entity/Instructor.java
+++ spring-hibernate-relations-one-to-many/src/hibernate/demo/entity/Instructor.java
@@ -1,5 +1,8 @@
 package hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -31,6 +35,11 @@ public class Instructor {
 	@OneToOne(cascade=CascadeType.ALL)
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructorDetail;
+	
+	@OneToMany(mappedBy="instructor", cascade= {
+			CascadeType.PERSIST, CascadeType.MERGE,
+			CascadeType.DETACH, CascadeType.REFRESH})
+	private List<Course> courses;
 	
 	public Instructor() {
 	}
@@ -86,8 +95,24 @@ public class Instructor {
 		return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
 				+ ", instructorDetail=" + instructorDetail + "]";
 	}
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
 	
-	
+	public void add(Course tempCourse) {
+		
+		if(courses == null)
+			courses = new ArrayList<>();
+		
+		courses.add(tempCourse);
+		
+		tempCourse.setInstructor(this);
+	}
 	
 	// annotate the class as an entity to map to a db table
 	
