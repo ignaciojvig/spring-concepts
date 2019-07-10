7e550f6d965f455a900e2d2e9e751e9fffd0af9d
João Victor Ignacio <joao.ignacio@venturus.org.br>
2019-07-10 11:02:46 -0300

    Deleting a student and removing from many-to-many table

---
+++ spring-hibernate-relations-many-to-many/src/hibernate/demo/DeleteMaryStudentDemo.java
@@ -0,0 +1,54 @@
+package hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import hibernate.demo.entity.Course;
+import hibernate.demo.entity.Instructor;
+import hibernate.demo.entity.InstructorDetail;
+import hibernate.demo.entity.Review;
+import hibernate.demo.entity.Student;
+
+public class DeleteMaryStudentDemo {
+
+	public static void main(String[] args) {
+		
+		// Create Session Factory
+		SessionFactory factory = new Configuration()
+											.configure("hibernate.cfg.xml")
+											.addAnnotatedClass(Instructor.class)
+											.addAnnotatedClass(InstructorDetail.class)
+											.addAnnotatedClass(Course.class)
+											.addAnnotatedClass(Review.class)
+											.addAnnotatedClass(Student.class)
+											.buildSessionFactory();
+		
+		// Create a Session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			// start a transaction
+			session.beginTransaction();
+			
+			// get the student 'Mary' from database
+			int studentId = 2;
+			
+			Student tempStudent = session.get(Student.class, studentId);
+			System.out.println("Student: " + tempStudent);
+			System.out.println("Courses: " + tempStudent.getCourses());
+			
+			System.out.println("Deleting student: " + tempStudent);
+			session.delete(tempStudent);
+			
+			// commit transaction
+			session.getTransaction().commit();
+		}
+		finally {
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
