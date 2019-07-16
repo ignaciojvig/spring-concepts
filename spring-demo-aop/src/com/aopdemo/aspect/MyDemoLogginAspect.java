002570b64d0af93da9656e312954f4312addcde7
João Victor Ignacio <joao.ignacio@venturus.org.br>
2019-07-16 16:24:57 -0300

    Aspect Demo using @Before advice

---
+++ spring-demo-aop/src/com/aopdemo/aspect/MyDemoLogginAspect.java
@@ -0,0 +1,16 @@
+package com.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLogginAspect {
+
+	// this is where all advices can be added
+	@Before("execution(public void addAccount())") // Pointcut Expression
+	public void beforeAddAccountAdvice() {
+		System.out.println("\n ==========> Executing @Before Advice on addAccount()");
+	}
+}
