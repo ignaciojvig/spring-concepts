81da8b7e5b5b0fc9cb7021e71e2b236c2af22b9d
João Victor Ignacio <Joo Victor Ignacio@DESKTOP-4GQEKLO>
2019-07-17 20:59:49 -0300

    Unfinished - Adding Params do Aspect

--- spring-demo-aop/src/com/aopdemo/dao/AccountDAO.java
+++ spring-demo-aop/src/com/aopdemo/dao/AccountDAO.java
@@ -2,10 +2,12 @@ package com.aopdemo.dao;
 
 import org.springframework.stereotype.Component;
 
+import com.aopdemo.Account;
+
 @Component
 public class AccountDAO {
 
-	public void addAccount() {
+	public void addAccount(Account theAccount) {
 		System.out.println(getClass() + " : Doing my DB Work - Add Account");
 	}
 }
