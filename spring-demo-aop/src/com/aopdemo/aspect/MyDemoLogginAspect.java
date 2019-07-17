986d88313cfa2c5dd474dc86d4674623c3807c2a
João Victor Ignacio <Joo Victor Ignacio@DESKTOP-4GQEKLO>
2019-07-17 15:22:57 -0300

    Run aspect on any method that starts with 'add' - Wildcard+PointcutExpre

--- spring-demo-aop/src/com/aopdemo/aspect/MyDemoLogginAspect.java
+++ spring-demo-aop/src/com/aopdemo/aspect/MyDemoLogginAspect.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 public class MyDemoLogginAspect {
 
 	// this is where all advices can be added
-	@Before("execution(public void com.aopdemo.dao.AccountDAO.addAccount())") // Pointcut Expression
+	@Before("execution(public void add*())") // Pointcut Expression
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n ==========> Executing @Before Advice on addAccount()");
 	}
