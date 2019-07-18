e95027e4560ca2c15446f049a77c96ed2bda0571
João Victor Ignacio <joao.ignacio@venturus.org.br>
2019-07-18 13:27:52 -0300

    Read method signature using Join Point

--- spring-demo-aop-reading-joinpoints/src/com/aopdemo/aspect/MyDemoLogginAspect.java
+++ spring-demo-aop-reading-joinpoints/src/com/aopdemo/aspect/MyDemoLogginAspect.java
@@ -1,7 +1,9 @@
 package com.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +14,15 @@ public class MyDemoLogginAspect {
 	
 	// this is where all advices can be added
 	@Before("com.aopdemo.aspect.PointcutExpressions.forDaoPackageWithoutGetterAndSetter()")
-	public void beforeAddAccountAdvice() {
-		System.out.println("\n ==========> Executing @Before Advice on addAccount()");
+	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+		
+		// Display the mthod signature
+		MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
+		
+		System.out.println("Method: " + methodSig);
+		
+		// Display method arguments
+		
 	}
 	
 }
