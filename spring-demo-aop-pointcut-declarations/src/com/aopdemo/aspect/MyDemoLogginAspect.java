239f0e62e71e64704c09f12286e8882db1c3a688
João Victor Ignacio <joao.ignacio@venturus.org.br>
2019-07-18 09:23:33 -0300

    Pointcut Declaration

--- spring-demo-aop-pointcut-declarations/src/com/aopdemo/aspect/MyDemoLogginAspect.java
+++ spring-demo-aop-pointcut-declarations/src/com/aopdemo/aspect/MyDemoLogginAspect.java
@@ -2,14 +2,18 @@ package com.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLogginAspect {
 
+	@Pointcut("execution(* com.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {};
+	
 	// this is where all advices can be added
-	@Before("execution(* com.aopdemo.dao.*.*(..))") // Pointcut Expression
+	@Before("forDaoPackage()") // Pointcut Expression
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n ==========> Executing @Before Advice on addAccount()");
 	}
