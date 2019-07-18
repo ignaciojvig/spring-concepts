cce58d502a30557b9cfa61f8975f43dd3bf21107
João Victor Ignacio <joao.ignacio@venturus.org.br>
2019-07-18 14:12:48 -0300

    Using after-returning to intercept the result

--- spring-demo-aop-after-returning/src/com/aopdemo/aspect/MyDemoLogginAspect.java
+++ spring-demo-aop-after-returning/src/com/aopdemo/aspect/MyDemoLogginAspect.java
@@ -1,6 +1,9 @@
 package com.aopdemo.aspect;
 
+import java.util.List;
+
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -13,6 +16,21 @@ import com.aopdemo.Account;
 @Component
 @Order(2)
 public class MyDemoLogginAspect {
+	
+	// add a new advice for @AfterReturning to the findAccounts method
+	@AfterReturning(
+			pointcut="execution(* com.aopdemo.dao.AccountDAO.findAccounts(..))",
+			returning="result")
+	public void afterReturningFindAccountsAdvice(
+			JoinPoint theJoinPoint, List<Account> result) {
+		
+		// print out which method we are advising on
+		String method = theJoinPoint.getSignature().toShortString();
+		System.out.println("*AfterReturning* on method: " + method);
+		
+		System.out.println("*AfterReturning* result is: " + result);
+		
+	}
 	
 	// this is where all advices can be added
 	@Before("com.aopdemo.aspect.PointcutExpressions.forDaoPackageWithoutGetterAndSetter()")
--- spring-demo-aop-after-returning/src/com/aopdemo/dao/AccountDAO.java
+++ spring-demo-aop-after-returning/src/com/aopdemo/dao/AccountDAO.java
@@ -1,5 +1,8 @@
 package com.aopdemo.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import com.aopdemo.Account;
@@ -10,6 +13,21 @@ public class AccountDAO {
 	private String name;
 	
 	private String serviceCode;
+	
+	public List<Account> findAccounts(){
+		
+		List<Account> myAccounts = new ArrayList<>();
+		
+		Account temp1 = new Account("John", "Silver");
+		Account temp2 = new Account("Thorin", "Oakenshield");
+		Account temp3 = new Account("Luca", "Gold");
+		
+		myAccounts.add(temp1);
+		myAccounts.add(temp2);
+		myAccounts.add(temp3);
+		
+		return myAccounts;
+	}
 	
 	public void addAccount(Account theAccount, boolean vipFlag) {
 		System.out.println(getClass() + " : Doing my DB Work - Add Account");
