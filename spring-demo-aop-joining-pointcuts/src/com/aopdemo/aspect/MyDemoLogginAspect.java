e229a9e26fe035a11475321bfec7e73dc71fd57d
João Victor Ignacio <joao.ignacio@venturus.org.br>
2019-07-18 09:58:08 -0300

    Setup of Project to study Pointcut Combinations

---
+++ spring-demo-aop-joining-pointcuts/src/com/aopdemo/aspect/MyDemoLogginAspect.java
@@ -0,0 +1,25 @@
+package com.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLogginAspect {
+
+	@Pointcut("execution(* com.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {};
+	
+	// this is where all advices can be added
+	@Before("forDaoPackage()")
+	public void beforeAddAccountAdvice() {
+		System.out.println("\n ==========> Executing @Before Advice on addAccount()");
+	}
+	
+	@Before("forDaoPackage()")
+	public void performApiAnalytics() {
+		System.out.println("\n ==========> Performing API Analytics");
+	}
+}
