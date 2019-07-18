5626e7a19e4e7c5eb8dadd521be736532a6917c7
João Victor Ignacio <joao.ignacio@venturus.org.br>
2019-07-18 10:04:46 -0300

    Renamed some projects adding 'demo' to their names

--- spring-demo-aop-joining-pointcuts/src/com/aopdemo/dao/AccountDAO.java
+++ spring-demo-aop-joining-pointcuts/src/com/aopdemo/dao/AccountDAO.java
@@ -7,7 +7,28 @@ import com.aopdemo.Account;
 @Component
 public class AccountDAO {
 
+	private String name;
+	
+	private String serviceCode;
+	
 	public void addAccount(Account theAccount, boolean vipFlag) {
 		System.out.println(getClass() + " : Doing my DB Work - Add Account");
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getServiceCode() {
+		return serviceCode;
+	}
+
+	public void setServiceCode(String serviceCode) {
+		this.serviceCode = serviceCode;
+	}
+
 }
