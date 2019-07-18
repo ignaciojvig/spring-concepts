69dd91406939772689f512561e780a24e24f2a3b
João Victor Ignacio <joao.ignacio@venturus.org.br>
2019-07-18 10:47:58 -0300

    Ordering Aspects with @Order

--- spring-demo-aop-pointcut-ordering-aspects/src/com/aopdemo/aspect/MyCloudLogAsyncAspect.java
+++ spring-demo-aop-pointcut-ordering-aspects/src/com/aopdemo/aspect/MyCloudLogAsyncAspect.java
@@ -2,10 +2,12 @@ package com.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(1)
 public class MyCloudLogAsyncAspect {
 
 	@Before("com.aopdemo.aspect.PointcutExpressions.forDaoPackageWithoutGetterAndSetter()")
--- spring-demo-aop-pointcut-ordering-aspects/src/com/aopdemo/aspect/MyDemoLogginAspect.java
+++ spring-demo-aop-pointcut-ordering-aspects/src/com/aopdemo/aspect/MyDemoLogginAspect.java
@@ -2,11 +2,12 @@ package com.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(2)
 public class MyDemoLogginAspect {
 	
 	// this is where all advices can be added
