a1dfd3fbe4b37ebcd44c09273df0e0772bf4d6c7
João Victor Ignacio <joao.ignacio@venturus.org.br>
2019-07-19 15:17:13 -0300

    Setup project to study @Around Advice

---
+++ spring-demo-aop-around/src/com/aopdemo/AfterReturningDemoApp.java
@@ -0,0 +1,29 @@
+package com.aopdemo;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.aopdemo.dao.AccountDAO;
+
+public class AfterReturningDemoApp {
+
+	public static void main(String[] args) {
+		
+		// read spring config class
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		// call method to find the accounts
+		List<Account> theAccounts = theAccountDAO.findAccounts(false);
+		
+		System.out.println("On Main\n" + theAccounts);
+		
+		// close the context
+		context.close();		
+	}
+
+}
---
+++ spring-demo-aop-around/src/com/aopdemo/AroundDemoApp.java
@@ -0,0 +1,29 @@
+package com.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.aopdemo.service.TrafficFortuneService;
+
+public class AroundDemoApp {
+
+	public static void main(String[] args) {
+		
+		// read spring config class
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get the bean from spring container
+		TrafficFortuneService theTrafficFortuneService = context.getBean("TrafficFortuneService", TrafficFortuneService.class);
+		
+		System.out.println("Calling Fortune");
+		
+		String fortune = theTrafficFortuneService.getFortune();
+		System.out.println("My Fortune is: " + fortune);
+		
+		System.out.println("Finished");
+		
+		// close the context
+		context.close();		
+	}
+
+}
