08d45de4cfd468790cb671329b2d62f9ca6c421a
João Victor Ignacio <joao.ignacio@venturus.org.br>
2019-07-24 18:20:04 -0300

    Added basic Security Configurations

---
+++ spring-security-basic-security/src/main/java/com/ignacio/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.ignacio.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
